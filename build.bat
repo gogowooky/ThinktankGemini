6c9436622170f7c9864e0adf509ee7e865fbf2ac
Shinichiro Egashira <gogowooky@gmail.com>
2025-12-04 23:47:43 +0900

    251204 2347 E15

---
+++ build.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell -ExecutionPolicy Bypass -File .\compile_test.ps1
+pause
--- script/DefaultActions.ps1
+++ script/DefaultActions.ps1
@@ -4,7 +4,7 @@ Add-TTAction    Application.Run.Break       'デバッグ画面で改行' {
     Write-Host 'Thinktank on Antigravity'
     [System.Windows.MessageBox]::Show("メッセージ本文", "タイトル")
 }
-#endregion
+
 
 Add-TTAction    Application.Operation.Quit  '終了' {
     $res = [System.Windows.MessageBox]::Show( "終了しますか", "QUIT", 'YesNo', 'Question')
